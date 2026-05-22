f82b3593aff70b4ff4cbc2e460574ef1d691bad3
KokoroRay <namnguyen123456cm@gmail.com>
2026-05-22 14:20:51 +0700

    bổ sung phần feedback logic

--- orcha-backend/pkg/models/feedback.go
+++ orcha-backend/pkg/models/feedback.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Feedback struct {
-	FeedbackId  string `json:"feedbackId" dynamodbav:"feedbackId"`
-	ProductId   string `json:"productId" dynamodbav:"productId"`
-	UserId      string `json:"userId" dynamodbav:"userId"`
-	UserName    string `json:"userName" dynamodbav:"userName"`
-	Rating      int    `json:"rating" dynamodbav:"rating"`
-	Comment     string `json:"comment" dynamodbav:"comment"`
-	ImageUrl    string `json:"imageUrl,omitempty" dynamodbav:"imageUrl,omitempty"`
-	CreatedAt   string `json:"createdAt" dynamodbav:"createdAt"`
+	FeedbackId string `json:"feedbackId" dynamodbav:"feedbackId"`
+	ProductId  string `json:"productId" dynamodbav:"productId"`
+	UserId     string `json:"userId" dynamodbav:"userId"`
+	UserName   string `json:"userName" dynamodbav:"userName"`
+	Rating     int    `json:"rating" dynamodbav:"rating"`
+	Comment    string `json:"comment" dynamodbav:"comment"`
+	ImageUrl   string `json:"imageUrl,omitempty" dynamodbav:"imageUrl,omitempty"`
+	CreatedAt  string `json:"createdAt" dynamodbav:"createdAt"`
 }
 
 type CreateFeedbackInput struct {
